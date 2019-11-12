d002a056a4e118d8b98888c467e286d236226724
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-11-12 12:15:20 +0000

    sample main hello world file

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+---------------------------------------------------
+Project: cpp_uservice_playground
+Timestamp: 2019-11-12 12:14
+Author: Manuel Bernal Llinares <mbdebian@gmail.com>
+---------------------------------------------------
+*/
+
+#include<iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
